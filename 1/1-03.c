9cdcfc932ba2e4e7a2cf6f66f035946e88305047
Luci <73762640+rresidentalien@users.noreply.github.com>
2024-02-27 17:36:49 +0100

    Update 1-03.c

--- 1/1-03.c
+++ 1/1-03.c
@@ -7,13 +7,19 @@ int main() {
     srand(time(NULL));
 
     int polje[32];
-    int i, n = 0;
+    int i;
+    int dekadski = 0;
 
     for (int i = 0; i < 32; ++i) {
         polje[i] = rand() % 2;
     }
 
-    for (i=32, i>=0; --i) {
+    for (i=32; i>=0; --i) {
         polje[i] = polje[i] * pow(2, 32-i);
+        dekadski+=polje[i];
     }
+    
+    printf("%d", dekadski);
+
+    return 0;
 }
