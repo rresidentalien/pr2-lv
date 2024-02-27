d23fe05492d10d8cc304a325ba22fa95ac8d177e
Luci <73762640+rresidentalien@users.noreply.github.com>
2024-02-27 17:45:27 +0100

    Create 1-04.c (unfinished)

---
+++ 1/1-04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    
+    int broj;
+    broj = (rand() % 65535) - 32767;
+    
+    int polje[32] = {0};
+    
+    
+}
