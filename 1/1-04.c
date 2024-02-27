21bd92477f6493b4f3d5ba0b68822b46ca36a19c
rresidentalien <73762640+rresidentalien@users.noreply.github.com>
2024-02-27 23:26:14 +0100

    finished 1-4

--- 1/1-04.c
+++ 1/1-04.c
@@ -5,10 +5,42 @@
 int main() {
     srand(time(NULL));
     
-    int broj;
+    int broj, i;
     broj = (rand() % 65535) - 32767;
     
-    int polje[32] = {0};
+    int bitovi[32] = {0};
     
-    
+    if (broj < 0) {
+        printf("1");
+        broj = -broj;
+
+        for (i=31; i>0; --i) { 
+            bitovi[i] = broj % 2;
+            broj = broj / 2;
+        }
+
+        for (i = 0; i < 32; ++i) {
+            bitovi[i] = (bitovi[i] == 0) ? 1 : 0;
+        }
+        for (i = 0; i < 32; ++i) {
+            if (bitovi[i] == 0) {
+                bitovi[i] = 1;
+            } else {
+                bitovi[i] = 0;
+            }
+        }
+    } else {
+        printf("0");
+
+        for (i=31; i>=0; --i) {
+            bitovi[i] = broj % 2;
+            broj = broj / 2;
+        }
+    }
+
+    for (i=0; i<32; ++i) {
+        printf("%d", bitovi[i]);
+    }
+
+    return 0;
 }
