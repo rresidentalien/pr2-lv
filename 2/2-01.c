1987b3cec44f3bdc48aa61eb93d60eb957779de5
rresidentalien <73762640+rresidentalien@users.noreply.github.com>
2024-02-28 22:10:21 +0100

    2/1,2

---
+++ 2/2-01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct {
+    float realni;
+    float imaginarni;
+} kompleksni;
+
+int main() {
+    kompleksni broj1, broj2, zbroj, razlika;
+
+    printf("Unesi prvi kompleksni broj: ");
+    scanf("%f %f", &broj1.realni, &broj1.imaginarni);
+    printf("Unesi drugi kompleksni broj: ");
+    scanf("%f %f", &broj2.realni, &broj2.imaginarni);
+
+    zbroj.realni = broj1.realni + broj2.realni;
+    zbroj.imaginarni = broj1.imaginarni + broj2.imaginarni;
+
+    razlika.realni = broj1.realni - broj2.realni;
+    razlika.imaginarni = broj1.imaginarni - broj2.imaginarni;
+
+    printf("REZULTATI:\n");
+    printf("Zbroj: %.3f, %.3f*i\n", zbroj.realni, zbroj.imaginarni);
+    printf("Razlika: %.3f, %.3f*i\n", razlika.realni, razlika.imaginarni);
+
+    return 0;
+}
---
+++ 2/2-02.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    unsigned short dan;
+    unsigned short mjesec;
+    unsigned short godina;
+}datum;
+
+struct podaci{
+    char ime[20];
+    char prezime[20];
+    int maticniBroj;
+    float prosjek;
+    datum datumRodjenja;
+}student[40];
+
+int main() {
+    int n, i;
+    int max = 0;
+    char ime[20], prezime[20];
+    scanf("%d", &n);
+
+    for (i=0; i<n; ++i) {
+        printf("Unesite ime:");
+        scanf("%s", student[i].ime);
+
+        printf("Unesite prezime:");
+        scanf("%s", student[i].prezime);
+
+        printf("Unesite maticni broj:");
+        scanf("%d", &student[i].maticniBroj);
+
+        printf("Unesite prosjek:");
+        scanf("%f", &student[i].prosjek);
+
+        printf("Unesite datum rodjenja:");
+        scanf("%hu.%hu.%hu.", &student[i].datumRodjenja.dan, &student[i].datumRodjenja.mjesec, &student[i].datumRodjenja.godina);
+
+        if (student[i].prosjek > max) {
+            max = student[i].prosjek;
+            strcpy(ime, student[i].ime);
+            strcpy(prezime, student[i].prezime);
+        }
+    }
+
+    printf("REZULTATI:\n");
+    printf("%s %s", ime, prezime);
+
+    return 0;
+}
